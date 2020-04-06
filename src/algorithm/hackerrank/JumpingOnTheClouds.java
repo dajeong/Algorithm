3dab4d4186e5bea3d24358d1188e5d1b1d56274d
dajeong <galnotepink@gmail.com>
2020-04-07 00:01:22 +0900

    Hackerrank - Practice > Algorithms > Implementation > Jumping on the Clouds

---
+++ src/algorithm/hackerrank/JumpingOnTheClouds.java
@@ -0,0 +1,26 @@
+package algorithm.hackerrank;
+
+import java.io.IOException;
+
+/**
+ * https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+ */
+public class JumpingOnTheClouds {
+
+    // Complete the sockMerchant function below.
+    static int jumpingOnClouds(int[] c) {
+        int jump = -1;
+        for (int i = 0; i < c.length; i++) {
+            jump++;
+            if (i + 2 < c.length && c[i + 2] == 0) {
+                i++;
+            }
+        }
+        return jump;
+    }
+
+    public static void main(String[] args) {
+        int result = jumpingOnClouds(new int[]{0, 0, 1, 0, 0, 1, 0});
+        System.out.println(result);
+    }
+}
